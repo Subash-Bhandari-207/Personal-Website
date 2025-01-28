8dd085f28694476323864407f41a63062b15376c
Subash Bhandari <subashbhandari11111@gmail.com>
2025-01-28 15:30:47 +0545

    commit

---
+++ start-server.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "C:\Users\Subash\Desktop\PORTFOLIO\materials\Node.js"   // Start Node.js server
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" "C:\Users\Subash\Desktop\PORTFOLIO\index.html"   // Open index.html in Chrome
